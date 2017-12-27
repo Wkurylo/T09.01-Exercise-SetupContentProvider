a23917625c27dc7d40e112f2bd1c3ab8f534cb9e
wkurylo88@gmail.com <wkurylo88@gmail.com>
2017-12-27 15:25:48 +0100

    * Android manifest permission

--- app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -19,11 +19,15 @@ package com.example.android.todolist.data;
 import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 
 // TODO (1) Verify that TaskContentProvider extends from ContentProvider and implements required methods
 public class TaskContentProvider extends ContentProvider {
+
+    private TaskDbHelper mTaskDbelper;
+    private SQLiteDatabase mSQLiteDatabase;
 
 
     /* onCreate() is where you should initialize anything you’ll need to setup
@@ -35,8 +39,9 @@ public class TaskContentProvider extends ContentProvider {
     public boolean onCreate() {
         // TODO (2) Complete onCreate() and initialize a TaskDbhelper on startup
         // [Hint] Declare the DbHelper as a global variable
+        mTaskDbelper = new TaskDbHelper(getContext());
 
-        return false;
+        return true;
     }
 
 
